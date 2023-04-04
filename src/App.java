93372586ed92c6173226afb5233c0c53cef048be
Javier Eduardo <javiereduardo55xd@gmail.com>
2023-04-04 14:49:14 -0700

    probando el rendimeinto con varios hilos

--- src/App.java
+++ src/App.java
@@ -1,16 +1,23 @@
 public class App {
     public static void main(String[] args) {
         // ticketH
-        leer leerTicketH1 = new leer("TicketH.csv", 0, 2, "ventas");
-        leer leerTicketH2 = new leer("TicketH.csv", 1, 2, "ventas");
-        leerTicketH1.start();
-        leerTicketH2.start();
+        // leer leerTicketH1 = new leer("TicketH.csv", 0, 2, "ventas");
+        // leer leerTicketH2 = new leer("TicketH.csv", 1, 2, "ventas");
+        // leerTicketH1.start();
+        // leerTicketH2.start();
         // ticketD
-        int total = 2;
+        int total = 3;
         leer[] leerTicketD = new leer[total];
         for (int i = 0; i < total; i++)
             leerTicketD[i] = new leer("TicketD.csv", i, total, "DetalleVentas");
         for (int i = 0; i < total; i++)
             leerTicketD[i].start();
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("___----.-.-.-.-");
+        System.exit(0);
     }
 }
